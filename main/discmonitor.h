e2a9d270504ea0d79358c27a8da1a87c8f935b40
Ithiliel <chan.yuzuki@googlemail.com>
2020-06-01 14:32:27 +0200

    Mehr Kommentare

--- main/discmonitor.h
+++ main/discmonitor.h
@@ -1,23 +1,24 @@
 #pragma once
 
+// Definition der Pins
 #include "pindef.h"
 
-#define NUM_SEGMENTS 12
+#define NUM_SEGMENTS 12 // Anzahl der schwarzen und weißen Segmente
 
 #define DROPDELAY 55000  // Gesamtverzögerung Auslösen - Aufprall in Microsekunden
 #define DROPTARGETPOS 50  // Position des Abwurfzieles innerhalb des Segmentes in Prozent
-#define HOLEPOS 0 //Nummer des Segmentes mit dem Loch
+#define HOLEPOS 0 // Nummer des Segmentes mit dem Loch
 
 class DiscMonitor
 {
   public:
-    void registerPhotoTransition();
-    void registerAbsolutePosition();
-    unsigned long getExpectedSegmentTime(int segment, int rev_ahead); 
-    bool getRecommendedTriggerPos(int *segment, unsigned long *triggerdelay); 
+    void registerPhotoTransition(); // verwurschteln des Photosensors
+    void registerAbsolutePosition(); // verwurschteln des Hallsensors
+    unsigned long getExpectedSegmentTime(int segment, int rev_ahead); // geschaetzte Dauer des Segments
+    bool getRecommendedTriggerPos(int *segment, unsigned long *triggerdelay); // Triggerposition bestimmen (wann soll die Kugel fallen gelassen werden)
 
-    volatile int currentPosition = 0; // Wirklich die derzeitige Position - also das Segment, das als nächstes verlassen wird
-      //Index Null = das Segment mit der Markierung der Absolutposition
+    volatile int currentPosition = 0; 	// Wirklich die derzeitige Position - also das Segment, das als nächstes verlassen wird
+										//Index Null = das Segment mit der Markierung der Absolutposition
     volatile unsigned long lastImpulseMicros = 0;
     volatile unsigned long segmentTimes[NUM_SEGMENTS] = {0};
     volatile bool absEdgeRegistered = false;
