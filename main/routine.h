7ab5a60f97a4565601ee03a81592b719fc5f1020
Ithiliel <chan.yuzuki@googlemail.com>
2020-06-03 22:22:22 +0200

    Declaration Routine (Try)

---
+++ main/routine.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// Definition der Pins
+#include "pindef.h"
+
+
+class Routine
+{
+  public:
+    void startFast(); // Fallsequenz fuer schnellen Modus starten
+	void startMedium(); // Fallsequenz fuer mittleren Modus starten
+    void startSlow(); // Fallsequenz fuer langsamen Modus starten
+};
