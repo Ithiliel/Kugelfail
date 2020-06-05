471a838fe716597ebd3e750ee8ce3d077ef35695
Ithiliel <chan.yuzuki@googlemail.com>
2020-06-05 13:22:07 +0200

    Routine ohne Geschwindigkeit
    Logik implementiert, Abfrage ob Geschwindigkeit passt fehlt noch

--- main/routine.cpp
+++ main/routine.cpp
@@ -10,4 +10,72 @@ bool goLittleBall(float speed){
 		return false;
 	}
 	// hier Test-Ende
+	
+	// TODO speed
+	if (modus == 0){ // Modus schnell
+		if (cnt > 4){ // dieser Fall solllte nicht auftreten, etwas ist schiefgegangen
+		    Serial.print(cnt);
+			Serial.println(" - Fehler in Modus Schnell");
+			return false;
+		}
+		
+		if (cnt = 4){ // letzte Kugel
+			cnt = 0;
+			modus = 1;
+			return true;
+		} else { // nicht letzte Kugel; alternierend fallen lassen und frei lassen
+			if (cnt%2 == 0){
+				cnt++;
+				return true;
+			} else {
+				cnt++;
+				return false;
+			}
+		}			
+		
+	} else if (modus == 1){ // Modus mittel
+		if (cnt = 8){ // letzte Kugel
+			cnt = 0;
+			modus = 2;
+			return true;
+		} else if (cnt < 8){ // nicht letzte Kugel
+			if (cnt==0 || cnt==2 || cnt==3 || cnt==6 || cnt==7){ // Fallen lassen!
+				cnt++;
+				return true;
+			} else { // nicht fallen lassen
+				cnt++;
+				return false;
+			}			
+			
+		} else {// dieser Fall solllte nicht auftreten, etwas ist schiefgegangen
+			Serial.print(cnt);
+			Serial.println(" - Fehler in Modus mittel");
+			return false;
+		}
+		
+		
+	} else if (modus == 2){ // Modus langsam
+		if (cnt = 2){ // letzte Kugel
+			cnt = 0;
+			modus = 3;
+			return true;
+		} else {
+			if (cnt < 2){
+				cnt ++;
+				return true;
+			} else { // dieser Fall solllte nicht auftreten, etwas ist schiefgegangen
+				Serial.print(cnt);
+				Serial.println(" - Fehler in Modus langsam");
+				return false;
+			}
+		
+	} else if (modus == 3){ // Modus fertig
+	
+		return false;
+		
+	} else{ // irgendwas ist schiefgegangen
+		Serial.print("Ups, unbekannter Modus");
+		return false;
+	}
+
 }
--- main/routine.h
+++ main/routine.h
@@ -8,5 +8,7 @@ class Routine
 {
   public:
 	bool goLittleBall(float speed); // Gibt zurück, ob die Kugel fallengelassen werden soll
-		
+	
+	volatile int modus = 0; // aktueller Modus (0:schnell, 1:mitel, 2:langsam, 3:fertig)
+	volatile int cnt = 0; //Zähler
 };
